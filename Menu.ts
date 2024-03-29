de1050e81b8366f27b08dd05258d48b6579c6132
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-03-29 10:49:01 -0300

    Update

--- Menu.ts
+++ Menu.ts
@@ -3,63 +3,29 @@ import { colors } from "./src/util/Cores";
 import { Conta } from "./src/model/Conta";
 import { ContaCorrente } from "./src/model/ContaCorrente";
 import { ContaPoupanca } from "./src/model/ContaPoupanca";
+import { ContaController } from "./src/controller/ContaController";
 
 export function main() {
 
     let opcao: number;
 
-    // // Instanciando um novo Objeto da Classe Conta
-    // console.log("\nCriar o Objeto da Classe Conta")
-    // let c1: Conta = new Conta(1, 123, 1, "Natasha", 1000000);
+    // Instância da Classe ContaController
+    const contas: ContaController = new ContaController();
 
-    // // Visualizando todos os dados da conta criada
-    // c1.visualizar();
+    // Instância da Classe Conta Corrente 
+    // Adicionada na Collection listaContas
+    const contaCorrente: ContaCorrente = new ContaCorrente(1, 456, 1, "Yasmine Lamark", 500000, 1000)
+    contas.cadastrar(contaCorrente);
 
-    // // Modificando o Saldo através do método set
-    // console.log("\nAlterar o Saldo para R$ 1500000.00")
-    // c1.saldo = 1500000;
-
-    // // Recuperando o valor do Saldo através do método get
-    // console.log(`\nNovo Saldo da Conta: ${c1.saldo}`);
-
-    // // Verificando se o Saque deu certo
-    // console.log(`\nSacar R$ 2000000.00 da conta: ${c1.sacar(2000000)}`);
-    // c1.visualizar();
-
-    // // Verificando se o Saque deu certo
-    // console.log(`\nSacar R$ 2000.00 da conta: ${c1.sacar(2000)}`);
-    // c1.visualizar();
-
-    // // Depositar dinheiro na conta
-    // console.log("\nDepositar R$ 5000.00 na Conta: ");
-    // c1.depositar(5000);
-    // c1.visualizar();
-
-    // Teste da Classe Conta Corrente
-
-    const cc1: ContaCorrente = new ContaCorrente(2, 456, 1, "Yasmine Lamark", 500000, 1000);
-    cc1.visualizar();
-
-    cc1.sacar(1000);
-
-    cc1.visualizar();
-
-    cc1.depositar(5000);
-
-    cc1.visualizar();
-
-    // Teste da Classe ContaPoupanca
-    const contapoupanca: ContaPoupanca = new ContaPoupanca(3, 123, 2, "Victor", 1000, 10);
-    contapoupanca.visualizar();
-    contapoupanca.sacar(200);
-    contapoupanca.visualizar();
-    contapoupanca.depositar(1000);
-    contapoupanca.visualizar();
+    // Instância da da Classe ContaPoupanca
+    // Adicionada na Collection listaContas
+    const contaPoupanca: ContaPoupanca = new ContaPoupanca(2, 123, 2, "Victor", 1000, 10);
+    contas.cadastrar(contaPoupanca);
 
     while (true) {
 
-        console.log(colors.bg.black, colors.fg.yellow, 
-                    "*****************************************************");
+        console.log(colors.bg.black, colors.fg.yellow,
+            "*****************************************************");
         console.log("                                                     ");
         console.log("                BANCO DO BRAZIL COM Z                ");
         console.log("                                                     ");
@@ -76,14 +42,14 @@ export function main() {
         console.log("            9 - Sair                                 ");
         console.log("                                                     ");
         console.log("*****************************************************");
-        console.log("                                                     ", 
-        colors.reset);
+        console.log("                                                     ",
+            colors.reset);
 
         console.log("Entre com a opção desejada: ");
         opcao = readlinesync.questionInt("");
 
         if (opcao == 9) {
-            console.log(colors.fg.greenstrong, 
+            console.log(colors.fg.greenstrong,
                 "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
             sobre();
             console.log(colors.reset, "");
@@ -92,55 +58,58 @@ export function main() {
 
         switch (opcao) {
             case 1:
-                console.log(colors.fg.whitestrong, 
+                console.log(colors.fg.whitestrong,
                     "\n\nCriar Conta\n\n", colors.reset);
-                
+
                 keyPress()
                 break;
             case 2:
-                console.log(colors.fg.whitestrong, 
+                console.log(colors.fg.whitestrong,
                     "\n\nListar todas as Contas\n\n", colors.reset);
+
+                // Chamada do Método listarTodas() da Classe ContaController
+                contas.listarTodas();
 
                 keyPress()
                 break;
             case 3:
-                console.log(colors.fg.whitestrong, 
+                console.log(colors.fg.whitestrong,
                     "\n\nConsultar dados da Conta - por número\n\n", colors.reset);
 
                 keyPress()
                 break;
             case 4:
-                console.log(colors.fg.whitestrong, 
+                console.log(colors.fg.whitestrong,
                     "\n\nAtualizar dados da Conta\n\n", colors.reset);
 
                 keyPress()
                 break;
             case 5:
-                console.log(colors.fg.whitestrong, 
+                console.log(colors.fg.whitestrong,
                     "\n\nApagar uma Conta\n\n", colors.reset);
 
                 keyPress()
                 break;
             case 6:
-                console.log(colors.fg.whitestrong, 
+                console.log(colors.fg.whitestrong,
                     "\n\nSaque\n\n", colors.reset);
 
                 keyPress()
                 break;
             case 7:
-                console.log(colors.fg.whitestrong, 
+                console.log(colors.fg.whitestrong,
                     "\n\nDepósito\n\n", colors.reset);
 
                 keyPress()
                 break;
             case 8:
-                console.log(colors.fg.whitestrong, 
+                console.log(colors.fg.whitestrong,
                     "\n\nTransferência entre Contas\n\n", colors.reset);
 
                 keyPress()
                 break;
             default:
-                console.log(colors.fg.whitestrong, 
+                console.log(colors.fg.whitestrong,
                     "\nOpção Inválida!\n", colors.reset);
 
                 keyPress()
---
+++ src/controller/ContaController.ts
@@ -0,0 +1,54 @@
+﻿import { Conta } from "../model/Conta";
+import { ContaRepository } from "../repository/ContaRepository";
+
+export class ContaController implements ContaRepository{
+    
+    // Collection Array que aramzenará os Objetos das Classes
+    // ContaCorrente e ContaPoupanca
+    private listaContas: Array<Conta> = new Array<Conta>();
+
+    // Atributo que será utilizado para controlar o numero das
+    // contas
+    public numero: number = 0;
+
+    procurarPorNumero(numero: number): void {
+        throw new Error("Method not implemented.");
+    }
+
+    // Método para Listar os dados de todas as Contas
+    // inseridas na Collection listaContas
+    listarTodas(): void {
+       for (let conta of this.listaContas){
+            conta.visualizar();
+       }
+    }
+
+     // Método para adicionar Objrtos das Classes 
+     // ContaCorrente e ContaPoupanca
+    // na Collection listaContas
+    cadastrar(conta: Conta): void {
+        this.listaContas.push(conta);
+        console.log("A Conta foi adicionada!")
+    }
+
+    atualizar(conta: Conta): void {
+        throw new Error("Method not implemented.");
+    }
+
+    deletar(numero: number): void {
+        throw new Error("Method not implemented.");
+    }
+
+    sacar(numero: number, valor: number): void {
+        throw new Error("Method not implemented.");
+    }
+
+    depositar(numero: number, valor: number): void {
+        throw new Error("Method not implemented.");
+    }
+
+    transferir(numeroOrigem: number, numeroDestino: number, valor: number): void {
+        throw new Error("Method not implemented.");
+    }
+    
+}
